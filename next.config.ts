de88687dcab53d6a1d1f65052a85121e3c940b50
ArhamImdad <119795504+ArhamImdad@users.noreply.github.com>
2026-07-04 04:54:03 +0500

    fix sitemap content-type

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
